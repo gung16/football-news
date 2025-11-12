0fe11615523f7ad5ca82ea429ef4059859e23206
gung16 <gung.160906@gmail.com>
2025-11-12 19:40:19 +0700

    Made changes

--- authentication/views.py
+++ authentication/views.py
@@ -5,7 +5,26 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 import json
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logged out successfully!"
+        }, status=200)
+    except:
+        return JsonResponse({
+            "status": False,
+            "message": "Logout failed."
+        }, status=401)
+    
 @csrf_exempt
 def register(request):
     if request.method == 'POST':
